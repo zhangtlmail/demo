6f89ffa7127d22ce5aa443c236b6971f054ae524
zhangtl <zhangtlmail@qq.com>
2020-03-01 12:30:18 +0800

    new branch commit

--- src/main/java/com/example/demo/controller/Aoverall.java
+++ src/main/java/com/example/demo/controller/Aoverall.java
@@ -8,26 +8,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.demo.pojo.Foo2;
+
 @Controller
 public class Aoverall {
-	public String drink(@Validated({Foo2.Adult.class}) Foo2 foo, BindingResult bindingResult) {
-	    if(bindingResult.hasErrors()){
-	        for (FieldError fieldError : bindingResult.getFieldErrors()) {
-	            //...
-	        }
-	        return "fail";
-	    }
-	    return "success";
+	public String drink(@Validated({ Foo2.Adult.class }) Foo2 foo, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			for (FieldError fieldError : bindingResult.getFieldErrors()) {
+				// ...
+			}
+			return "fail";
+		}
+		return "success";
 	}
 
 	public String live(@Validated Foo2 foo, BindingResult bindingResult) {
-	    if(bindingResult.hasErrors()){
-	        for (FieldError fieldError : bindingResult.getFieldErrors()) {
-	            //...
-	        }
-	        return "fail";
-	    }
-	    return "success";
+		if (bindingResult.hasErrors()) {
+			for (FieldError fieldError : bindingResult.getFieldErrors()) {
+				// ...
+			}
+			return "fail";
+		}
+		return "success";
+	}
+
+	public void branch() {
+		System.out.println("new hot_fix");
 	}
 
 }
